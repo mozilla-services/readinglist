8684abde1b183b018cbd97e2db04891dbd21de89
Mathieu Leplatre <mathieu@mozilla.com>
2015-03-25 23:23:41 +0100

    Back to development: 1.3.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,6 +3,12 @@ Changelog
 
 This document describes changes between each past release.
 
+1.3.1 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 1.3.0 (2015-03-25)
 ------------------
 
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ ENTRY_POINTS = {
     ]}
 
 setup(name='readinglist',
-      version='1.3.0',
+      version='1.3.1.dev0',
       description='readinglist',
       long_description=README + "\n\n" + CHANGELOG,
       license='Apache License (2.0)',
